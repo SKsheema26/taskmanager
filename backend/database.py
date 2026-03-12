f6e2c8a700d556254e41d1f668942d0dc9b54680
SK.Sheema <155035517+sk-sheema424@users.noreply.github.com>
2026-03-12 15:31:41 +0530

    Switch to PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./taskmanager.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./taskmanager.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+if DATABASE_URL.startswitch("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://". "postgressql://", 1)
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
